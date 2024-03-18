0fb4c0201431fb5ee54eb8df4db08379777a7f6d
suzan <sznghimire61@gmail.com>
2024-03-18 22:38:08 +0545

    little fix in footer part

--- client/view/Footer.php
+++ client/view/Footer.php
@@ -7,15 +7,18 @@
     <style>
         /* Styling for the footer */
         footer {
-            background-color: #fff9ee;
-            color: black;
+            background-color: #FFF5E0;
+            /*color: black;*/
             text-align: center;
             padding: 20px 0;
-            margin-top: 20px;
             position: fixed;
             bottom: 0;
             left: 0;
+            height: 10px;
             width: 100%;
+            background-color: rgba(255, 255, 255, 1);
+            color: #000;
+            box-shadow: 0px -25px 20px -17px rgba(0,0,0,0.45);
         }
     </style>
 </head>
--- partner/views/Footer.php
+++ partner/views/Footer.php
@@ -8,13 +8,17 @@
         /* Styling for the footer */
         footer {
             background-color: #FFF5E0;
-            color: black;
+            /*color: black;*/
             text-align: center;
             padding: 20px 0;
             position: fixed;
             bottom: 0;
             left: 0;
+            height: 10px;
             width: 100%;
+            background-color: rgba(255, 255, 255, 1);
+            color: #000;
+            box-shadow: 0px -25px 20px -20px rgba(0,0,0,0.45);
         }
     </style>
 </head>
